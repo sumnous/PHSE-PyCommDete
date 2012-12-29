554068d456eacdf82e1d577ef74003ad5ab82304
nourlcn <nourlcn@gmail.com>
2012-12-29 13:35:22 +0800

    new commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 __author__ = 'nourl'
 
 
-input_type = 2 #1:real gml; 2: benchmark
+input_type = 1 #1:real gml; 2: benchmark
 file_num = 1 #0:polbooks;1:karate;2:dolphins;3:netscience
 seeds_type = 2 #1:degree_clique hyper ; 2: betweenness_clique hyper; 3: degree_betweenness hyper
 alpha = 1.0
