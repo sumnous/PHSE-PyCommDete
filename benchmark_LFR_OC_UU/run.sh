0b6ce8aa5a1bb05dda40c042fb7332a486dfac8b
nourlcn <nourlcn@gmail.com>
2013-01-02 03:35:44 +0800

    LFM run.sh error: no PyCommDete Modual

---
+++ benchmark_LFR_OC_UU/run.sh
@@ -0,0 +1,29 @@
+#for i in `seq 2 1 8`;do
+for i in 2;do
+	#echo $i
+	rm flags.dat
+	cat base.dat >> flags.dat
+	echo "-om "$i >> flags.dat
+	cat tail.dat >> flags.dat
+
+	make
+       	./benchmark -f flags.dat
+
+	cd ../
+	cd ./lfm
+
+	#python2 SeedDrivenDete.py >> ./log/$i.log
+	#python2 ./gce/GCE.py >> ./log/$i.log
+	python2 LFM.py >> .././log/$i.log
+
+	cd ../
+	cd ./evaluations/mutual3
+	#python2 NMI.py >> $i.nmi.log
+	echo "i ="$i": " >> ../../log/$i.nmi.log
+	#./mutual true.dat result.dat >> ../../log/$i.nmi.log
+	#./mutual true.dat result_GCE.dat >> ../../log/$i.nmi.log
+	./mutual true.dat result_LFM.dat >> ../../log/$i.nmi.log
+
+	cd ../../benchmark_LFR_OC_UU
+
+done
--- lfm/LFM.py
+++ lfm/LFM.py
@@ -13,19 +13,72 @@ def pick_random_node(nodes):
     random_node = nodes[random_num]
     return random_node
 
+def get_all_nature_community_multiprocess(left_nodes_list):
+    random_nodes = []
+
+    pool_result = []
+    global process_num
+    if len(left_nodes_list) < process_num:
+        process_num = len(left_nodes_list)
+
+    for i in range(process_num):
+        random_node = pick_random_node(left_nodes_list)
+        random_nodes.append([random_node])
+        left_nodes_list.remove(random_node)
+    print "print random_nodes:", random_nodes
+
+    pool = Pool(process_num)
+
+    group_list = split_list(random_nodes, process_num)
+
+    for i in range(1, len(group_list), 2):
+        group_list[i].reverse()
+
+    for i in range(process_num):
+        args=[]
+        for gr in group_list:
+            if len(gr)>i:
+                args.append(gr[i])
+        args=(args,)
+        #		args=([random_nodes[j] for j in range(process_num) if j%process_num==i],)
+        print "args__________", args
+        pool_result.append(pool.apply_async(process_f,args))
+
+    pool.close()
+    pool.join()
+
+    communities=[]
+    for x in pool_result:
+        communities = communities+x.get()
+    print "finish process_num processes get single node nature community___________________"
+    return communities
+
 def get_all_nature_community_LFM(network):
     all_nodes = network.nodes()
     left_nodes_list = all_nodes
     communities = []
     while left_nodes_list != []:
-        tem_node = pick_random_node(left_nodes_list)
-        community = get_nature_community_short(nx.Graph(C.subgraph(tem_node)))
-        communities.append(community)
-        if community != network:
-            for x in left_nodes_list:
-                if x in community.nodes():
-                    left_nodes_list.remove(x)
+        tem_communities = get_all_nature_community_multiprocess(left_nodes_list)
+        communities = communities + tem_communities
+        for x in tem_communities:
+            if x != network:
+                for y in x.nodes():
+                    if y in left_nodes_list:
+                        left_nodes_list.remove(y)
+        print "length of left_nodes_list__________: ", len(left_nodes_list)
     return communities
+
+def deal_communities_LFM(communities):
+    len_comm = len(communities)
+    fits = map(get_fitness, communities)
+    fits_dic = {}
+    for x in range(len_comm):
+        fits_dic[communities[x]] = fits[x]
+    print "fits_dic",len(fits_dic)
+    ave_fits = sum(fits)/len_comm
+    result = filter(lambda x:fits_dic[x]>ave_fits, communities)
+    print "above ave fitness communities", len(result), [fits_dic[x] for x in result]
+    return result
 
 if __name__ == "__main__":
     import time
@@ -49,8 +102,11 @@ if __name__ == "__main__":
     #	print "seeds_deal", seeds_deal
 
     communities = get_all_nature_community_LFM(C)
-    print "commplete get_all_nature_community"
-    results = deal_communities(communities)
+    print "commplete get_all_nature_community, length: ",len(communities)
+    communities = deal_communities(communities)
+    print "after deal_communities, length:______________", len(communities)
+    results = deal_communities_LFM(communities)
+
 
     f = file(base +'/evaluations/mutual3/result_LFM.dat', 'w+')
     for line in results:
