179fbf96dba9123d8741dc2d8e26460d0fe2dfa8
nourlcn <nourlcn@gmail.com>
2013-01-04 10:42:57 +0800

    add suspicious abandon seed method in GCE.py

--- config/com.py
+++ config/com.py
@@ -6,7 +6,7 @@ filelist=[base +'/inputs/GML/polbooks.gml',\
           base +'/inputs/GML/netscience.gml']
 
 input_type = 2 #1:real gml; 2: benchmark
-file_num = 1 #0:polbooks;1:karate;2:dolphins;3:netscience
+file_num = 2 #0:polbooks;1:karate;2:dolphins;3:netscience
 seeds_type = 2 #1:degree_clique hyper ; 2: betweenness_clique hyper; 3: degree_betweenness hyper
 alpha = 1.0
 beta = 0.6
@@ -17,6 +17,7 @@ avg_type = 1 #0:3 1:4 2:avg 3:junfangcha  MinSeedSize for downsides_seeds()
 dis_threshold = 0.25 #small than dis_threshold, then delete this community
 cch_threshold = 0.25 #similarity more than 1-cch_threshold in at least 2 already accepted seeds, then delete this clique
 # #clique coverage heuristic
+delta_threshold = 0.6
 
-process_num=32
+process_num=1
 thread_num=4
