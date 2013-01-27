5e1027e5033aa9e5238f1d46763506a266156516
nourlcn <nourlcn@gmail.com>
2013-01-27 13:55:56 +0800

    fix

--- benchmark_LFR_OC_UU/run.sh
+++ benchmark_LFR_OC_UU/run.sh
@@ -1,9 +1,9 @@
-for i in `seq 1 1 8`;do
-#for i in 8;do
+for i in `seq 0.10 0.05 0.95`;do
+#for i in 0.05;do
 	#echo $i
 	rm flags.dat
 	cat base.dat >> flags.dat
-	echo "-om "$i >> flags.dat
+	echo "-mu "$i >> flags.dat
 	cat tail.dat >> flags.dat
 
 	make
@@ -13,14 +13,14 @@ for i in `seq 1 1 8`;do
 
 	python2 SeedDrivenDete.py >> ./log/$i.log
 	#python2 GCE.py >> ./log/$i.log
-	#python2 LFM.py >> ./log/$i.log
+	#python2 LFM.py >> ./log/$i.lfm.log
 	
 	cd ./evaluations/mutual3
 	#python2 NMI.py >> $i.nmi.log
 	echo "i ="$i": " >> ../../log/nmi.log
 	./mutual true.dat result.dat >> ../../log/nmi.log
 	#./mutual true.dat result_GCE.dat >> ../../log/nmi.log
-	#./mutual true.dat result_LFM.dat >> ../../log/nmi.log
+	#./mutual true.dat result_LFM.dat >> ../../log/nmi.lfm.log
 
 	cd ../../benchmark_LFR_OC_UU
 
